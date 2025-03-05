bf4b391463daeb48559649cb9a515f1dc03d0d2e
owner <miriam.havin@mail.huji.ac.il>
2025-03-05 17:01:04 +0200

    Merge remote-tracking branch 'origin/master'

--- encoding/ridge_utils/stimulus_utils.py
+++ encoding/ridge_utils/stimulus_utils.py
@@ -10,8 +10,8 @@ def load_textgrids(stories, data_dir: str):
     grids = {}
     for story in stories:
         grid_path = os.path.join(base, "%s.TextGrid"%story)
-        print(grid_path)
         grids[story] = TextGrid(open(grid_path).read())
+        print(grids[story])
     return grids
 
 
