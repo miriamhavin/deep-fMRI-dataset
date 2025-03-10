160d4e5fdfe5e0275f9d1cfa65f91b47fc71cebb
owner <miriam.havin@mail.huji.ac.il>
2025-03-10 11:09:58 +0200

    remove unnecassary prints

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -66,7 +66,12 @@ if __name__ == "__main__":
 	print("delPstim: ", delPstim.shape)
 	# Add this near the beginning of your script
 	print(f"Rstim unique values: {len(np.unique(delRstim))}")
-	print(f"Rresp unique values: {len(np.unique(delRstim))}")
+	print(f"Rresp unique values: {len(np.unique(delPstim))}")
+	col_var = np.var(delRstim, axis=0)
+	print(f"Min column variance: {np.min(col_var)}")
+	print(f"Max column variance: {np.max(col_var)}")
+	print(f"Variance percentiles: {np.percentile(col_var, [10, 50, 90])}")
+
 	# Response
 	zRresp = get_response(train_stories, subject)
 	print("zRresp: ", zRresp.shape)
