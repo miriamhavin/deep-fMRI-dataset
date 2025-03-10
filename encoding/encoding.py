9240f61b9b691cea9ec74e7fe0ef428ec04714ba
owner <miriam.havin@mail.huji.ac.il>
2025-03-10 21:30:08 +0200

    commit

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -71,9 +71,6 @@ if __name__ == "__main__":
 	print("zRresp: ", zRresp.shape)
 	zPresp = get_response(test_stories, subject)
 	print("zPresp: ", zPresp.shape)
-	zRresp_trimmed = zRresp[-delRstim.shape[0]:, :]
-	zPresp_trimmed = zPresp[-delPstim.shape[0]:, :]
-	print("zRresp_trimmed: ", zRresp_trimmed.shape)
 
 
 	# Filter constant voxels
