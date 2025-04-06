4f4122cea5ec8e3beb01641bff8cfd24d4d9d521
owner <miriam.havin@mail.huji.ac.il>
2025-04-06 12:04:02 +0300

    Your commit message here

--- encoding/config.py
+++ encoding/config.py
@@ -5,5 +5,5 @@ from os.path import join, dirname
 
 REPO_DIR = join(dirname(dirname(os.path.abspath(__file__))))
 EM_DATA_DIR = join(REPO_DIR, 'em_data')
-DATA_DIR = join(REPO_DIR, 'data')
+DATA_DIR = join('/sci/labs/arielgoldstein/miriam1234')
 
